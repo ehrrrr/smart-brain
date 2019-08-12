b96f1ed346872c39165e0c33cdbffe2758c0ace5
ehrrrr <elisaveta.s.hristova@gmail.com>
2019-08-12 19:10:32 +0300

    Updates

---
+++ precache-manifest.97e035aa6254cb16a1cf3e7a9f10108f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467657aa1cae8f87ebeb83d3b12af9ac",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "d3ab51ce0d045640b8e3",
+    "url": "/smart-brain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "0bf428ac87099281ea53",
+    "url": "/smart-brain/static/css/main.ad129ae0.chunk.css"
+  },
+  {
+    "revision": "d3ab51ce0d045640b8e3",
+    "url": "/smart-brain/static/js/2.4f3de748.chunk.js"
+  },
+  {
+    "revision": "0bf428ac87099281ea53",
+    "url": "/smart-brain/static/js/main.5c638d25.chunk.js"
+  },
+  {
+    "revision": "11eaf24a56b84c9e227a",
+    "url": "/smart-brain/static/js/runtime~main.9947bed8.js"
+  },
+  {
+    "revision": "cf1bd1d39dc877669241faf6a03e3d72",
+    "url": "/smart-brain/static/media/brain.cf1bd1d3.png"
+  }
+]);
