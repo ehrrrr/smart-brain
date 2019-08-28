b342b7eed8fc1fca4e949daaace7e50ef5ef7b40
ehrrrr <elisaveta.s.hristova@gmail.com>
2019-08-28 18:31:19 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
 
 	onButtonSubmit = () => {
 		this.setState({ imageUrl: this.state.input });
-		fetch('http://localhost:3000/imageurl', {
+		fetch('https://warm-stream-64020.herokuapp.com/imageurl', {
 			method: 'post',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
@@ -104,7 +104,7 @@ class App extends Component {
 			.then((response) => response.json())
 			.then((response) => {
 				if (response) {
-					fetch('http://localhost:3000/image', {
+					fetch('https://warm-stream-64020.herokuapp.com/image', {
 						method: 'put',
 						headers: { 'Content-Type': 'application/json' },
 						body: JSON.stringify({
